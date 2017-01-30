bbd7fd085e90d791c2cd8c5b101100f564b6fff0
shudock <sammihudock@lawn-128-61-59-173.lawn.gatech.edu>
2017-01-29 21:18:14 -0500

    Committing Person4

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Sammi Hudock
  *  @version 1.1
  */
 public class Person4 {
